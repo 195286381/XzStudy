fb045bb1dbb3db44617dc22c2a16d93993ca5130
XiaoZhu1990 <195286381@qq.com>
2016-11-10 10:37:45 +0800

    新增去掉严格模式下的情况

--- JavaScript高级程序设计/第六章 面向对象的程序设计/Object_defineProperty.js
+++ JavaScript高级程序设计/第六章 面向对象的程序设计/Object_defineProperty.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'; // 设置全局严格模式
 var c = {};
 
 Object.defineProperty(c, 'say', {
@@ -6,6 +6,9 @@ Object.defineProperty(c, 'say', {
     value: 'I say Hi.'
 });
 
-// 此时代码应该会报错, 因为你设置了 writable = false 该属性为只读属性,不能修改
+// 此时代码应该会报错, 因为你设置了 writable = false, 该属性为只读属性,不能修改
 // TypeError: Cannot assign to read only property 'say' of #<Object>
 c.say = 'I say Hi~~~';
+
+// 在非严格模式下, 因为你设置了 writable = false, 该属性为只读属性 修改无效 输出依旧为之前的赋值
+console.log(c.say);
