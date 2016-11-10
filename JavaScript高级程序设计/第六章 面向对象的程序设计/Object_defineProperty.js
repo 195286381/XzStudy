38f25ce1d248a4279ec518974fd30e026eff2001
XiaoZhu1990 <195286381@qq.com>
2016-11-10 14:27:50 +0800

    添加信息到github

--- JavaScript高级程序设计/第六章 面向对象的程序设计/Object_defineProperty.js
+++ JavaScript高级程序设计/第六章 面向对象的程序设计/Object_defineProperty.js
@@ -1,6 +1,8 @@
 'use strict'; // 设置全局严格模式
 var c = {};
 
+// object.defineProperty(obj, prop, descriptor); 为一个对象定义一个新的属性
+// object.defineProperty(obj, descriptors); 为一个对象定义多个属性
 Object.defineProperty(c, 'say', {
     writable: false,
     value: 'I say Hi.'
@@ -12,3 +14,5 @@ c.say = 'I say Hi~~~';
 
 // 在非严格模式下, 因为你设置了 writable = false, 该属性为只读属性 修改无效 输出依旧为之前的赋值
 console.log(c.say);
+
+
