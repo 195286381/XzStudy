f90fd7c62c5147e9711183695615c4b39408b849
X_zzzzz <195286381@qq.com>
2016-11-14 14:49:08 +0800

    Create Currying_Demo.js

---
+++ JavaScript函数式编程/Code-Practice/Currying_Demo.js
@@ -0,0 +1,25 @@
+// 柯里化（Currying）是把接受多个参数的函数变换成接受一个单一参数(最初函数的第一个参数)的函数，
+// 并且返回接受余下的参数且返回结果的新函数的技术
+
+// 实现柯里化的Func
+function  currying(func) {
+	var args = Array.prototype.slice.call(arguments,1);
+	return function () {
+		var newArgs = args.concat(Array.prototype.slice.call(arguments));
+		return func.apply(null, newArgs);
+	}
+}
+
+function add() {
+	var total = 0;
+
+	for (var i = 0; i < arguments.length; i++) {
+		total = total + arguments[i];
+	}
+
+	return total;
+}
+
+var addWithCurry = curryingFun(add, 1, 2, 4);
+
+console.log(addWithCurry(123)); // => 130
