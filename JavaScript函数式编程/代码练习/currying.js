7959b889b506deb13d549aa58adece7dd76991c4
XiaoZhu1990 <195286381@qq.com>
2017-02-07 08:42:50 +0800

    提交代码

--- JavaScript函数式编程/代码练习/currying.js
+++ JavaScript函数式编程/代码练习/currying.js
@@ -1,3 +1,21 @@
+/**
+ * @fileoverview 描述页面信息
+ * @author xzzzzz<195286381@qq.com>
+ * @version v.0.0.1
+ */
+
+
+/**
+ * @fileoverview
+ * @param fun
+ * @returns {Function}
+ */
+
+function addYourName() {
+	var args = Array.prototype.slice.call(arguments, 1);
+	var black = console.log('this is a error');
+}
+
 function currying (fun) {
 	var args = Array.prototype.slice.call(arguments,1);
 
@@ -5,5 +23,28 @@ function currying (fun) {
 		var newArgs = args.concat(Array.prototype.slice.call(arguments));
 
 		return fun.apply(null, newArgs);
-	}
+	};
 }
+/**
+ * @function 返回匿名函数求和
+ * @returns {Function}
+ */
+function addYourName() {
+	var args = Array.prototype.slice.call(arguments, 1);
+	return function () {
+		return args;
+    };
+}
+
+/**
+ * @func
+ * @description 一个带参数的函数
+ * @param {string} a
+ * @param {string} black
+ * @param {string} conso
+ * @returns {string}
+ */
+function add(a, black, conso) {
+	return 'hello';
+}
+
