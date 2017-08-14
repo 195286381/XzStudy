7fb9befe9d1d6060d873c3f7bfdd7bb163a54f62
X_zzzzz <195286381@qq.com>
2017-08-14 11:17:01 +0800

    Create class_object.js

---
+++ 代码草稿纸/class_object.js
@@ -0,0 +1,42 @@
+/**
+ * create by Xzzzz in 14/08/2017
+ */
+;(function(name, factory, undefined) {
+  var isBrowserEnv = typeof window === 'object';
+  if (typeof module === 'object' && typeof module.exports === 'function') {
+    module.exports = factory();
+  } else if (typeof require === 'function') {
+      require(factory)
+  } else if (isBrowserEnv) {
+      window[name] = factory();
+  } else {
+    throw new Error();
+  }
+})('__tools__', function() {
+  var object = {
+    Class: function() {
+      var P = arguments[0];
+      console.log(typeof P.initilize)
+      var c = typeof P.initilize === 'function' ? P.initilize : function() {};
+      c.prototype = P;
+      return c;
+    }
+  }
+  return object;
+});
+
+// DEMO:
+// 
+// var Obj = root.__tools__.Class({
+//   initilize: function(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   },
+//   say: function() {
+//     var name = this.name;
+//     var age = this.age;
+//     console.log(`My name is ${name}, I am ${age} years old!`)
+//   }
+// })
+// var obj = new Obj('xzzzz', 24);
+// obj.say();
